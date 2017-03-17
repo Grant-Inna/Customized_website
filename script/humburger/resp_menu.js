6d534adddde8367cde5cb80aec4e67b92c9f9997
grantinna <grantinna@mail.ru>
2017-03-18 01:59:45 +0300

    Making adaptive menu with jquery

--- script/humburger/resp_menu.js
+++ script/humburger/resp_menu.js
@@ -1,9 +1,11 @@
 $(function () {
 
     $('.menuToggle').on('click', function () {
-        $('.navigationWrapper__listContainer').slideToggle(300, function () {
-
-        })
-    })
+        $('.navigationWrapper__listContainer').slideToggle(250, function () {
+            if ($(this).css('display') == 'none') { console.log($(this).css('display'));
+                $(this).removeAttr('style');
+            }
+        });
+    });
 
 });
