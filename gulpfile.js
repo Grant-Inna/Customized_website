fd0e68cec0e3e7eaec1ec3904db6836dbe267b8b
grantinna <grantinna@mail.ru>
2017-03-23 01:39:40 +0300

    Продолжаем работать с gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,35 @@ var gulp = require('gulp'),
     notify = require("gulp-notify"),
     rename = require("gulp-rename"),
     autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
     connect = require('gulp-connect');
 
 gulp.task('css', function() {
-    return gulp.src('dev/allStyle.css')
-        .pipe(autoprefixer({browsers: ['last 5 versions']}))
+    return gulp.src('./dev/allStyle.css')
+        .pipe(autoprefixer({browsers: ['last 5 versions', '> 5%']}))
         .pipe(cleanCSS())
         .pipe(rename("style.min.css"))
-        .pipe(gulp.dest('style/'))
+        .pipe(gulp.dest('app/style/'))
+        .pipe(connect.reload())
         .pipe(notify('Success!'));
 });
 
-gulp.task('watch', function() {
-   gulp.watch('dev/allStyle.css', ['default']);
+gulp.task('html', function() {
+    gulp.src('app/index.html')
+    .pipe(connect.reload());
+
 });
 
+gulp.task('connect', function() {
+    connect.server({
+        root: 'app/',
+        livereload: true
+    });
+});
 
-gulp.task('default', ['css', 'watch']);
+gulp.task('watch', function() {
+   gulp.watch('dev/allStyle.css', ['css'])
+   gulp.watch('app/index.html', ['html'])
+});
+
+gulp.task('default', ['css', 'html', 'watch', 'connect']);
