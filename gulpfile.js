98ab8da3225d9390f97ed4fdcfaab39a55bbdac7
grantinna <grantinna@mail.ru>
2017-03-23 01:05:14 +0300

    Добавляем различные таски для проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     notify = require("gulp-notify"),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    autoprefixer = require('gulp-autoprefixer'),
+    connect = require('gulp-connect');
 
-gulp.task('default', function() {
+gulp.task('css', function() {
     return gulp.src('dev/allStyle.css')
+        .pipe(autoprefixer({browsers: ['last 5 versions']}))
         .pipe(cleanCSS())
         .pipe(rename("style.min.css"))
         .pipe(gulp.dest('style/'))
@@ -14,3 +17,6 @@ gulp.task('default', function() {
 gulp.task('watch', function() {
    gulp.watch('dev/allStyle.css', ['default']);
 });
+
+
+gulp.task('default', ['css', 'watch']);
